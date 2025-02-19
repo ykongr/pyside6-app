858a2e6f42c916499444f794c647ca6a191fc6d2
ykongr <rd23052q@st.omu.ac.jp>
2025-02-20 02:40:46 +0900

    ラベル要素の実装

--- main.py
+++ main.py
@@ -1,1 +1,81 @@
-print("hello_world")
+import sys
+import PySide6.QtCore as Qc
+import PySide6.QtWidgets as Qw
+import subprocess
+import json
+
+def load_json(filename):
+    with open(filename, 'r', encoding='utf-8') as f:  # 'r'モードでファイルを開く
+        data = json.load(f)  # JSONデータをPythonのデータ構造に変換
+    return data
+data = load_json('data.json')
+
+
+class MainWindow(Qw.QMainWindow):
+  
+  def __init__(self):
+
+    super().__init__() 
+    self.setWindowTitle('MainWindow') 
+    self.setGeometry(100, 50, 640, 320) 
+
+    self.input_field = Qw.QLineEdit(self)
+    self.input_field.setPlaceholderText("リスト名を入力してください")
+    self.input_field.setVisible(False)
+
+    self.btn_list = Qw.QPushButton('リスト名を変更',self)
+    self.btn_list.setGeometry(10,10,100,40)
+    self.btn_list.clicked.connect(self.btn_list_clicked)
+
+
+    self.btn_add = Qw.QPushButton('要素を追加',self)
+    self.btn_add.setGeometry(120,10,100,40)
+    # self.btn_add.clicked.connect(self.btn_add_clicked)
+
+    self.btn_reload = Qw.QPushButton('更新',self)
+    self.btn_reload.setGeometry(230,10,100,40)
+    self.btn_reload.clicked.connect(self.btn_reload_clicked)
+
+    self.tb_log = Qw.QTextEdit('',self)
+    self.tb_log.setStyleSheet("{background-color: white;}")
+    self.tb_log.setGeometry(10,50,620,265)
+    self.tb_log.setReadOnly(True)
+    self.labels = []
+
+    for i in range(len(data["list_data"])):
+      self.list_name = Qw.QLabel(data["list_data"][i],self)
+      self.list_name.setStyleSheet("font-size: 16px; color: gray;")
+      self.list_name.setGeometry(20+i*100,50,100,20)
+      self.list_name.show()
+      self.labels.append(self.list_name)
+      
+
+  def btn_list_clicked(self):
+    subprocess.Popen([sys.executable, 'sab.py'])
+
+  def btn_reload_clicked(self):
+    self.update_labels()
+
+  def update_labels(self):
+    data = load_json('data.json')
+    print(data["list_data"])
+
+    for i in range(len(data["list_data"])):
+      blank = Qw.QLabel("                    ",self)
+      new_label = Qw.QLabel(data["list_data"][i], self)
+      new_label.setStyleSheet("font-size: 16px; color: gray;")
+      blank.setStyleSheet("background-color: white;font-size: 16px; color: gray;")
+      blank.setGeometry(20 + i * 100, 50, 100, 20)
+      new_label.setGeometry(20 + i * 100, 50, 100, 20)
+      blank.show()
+      new_label.show()
+
+
+      
+      
+
+if __name__ == '__main__':
+  app = Qw.QApplication(sys.argv)
+  main_window = MainWindow()
+  main_window.show()
+  sys.exit(app.exec())
---
+++ sab.py
@@ -0,0 +1,68 @@
+import sys
+import PySide6.QtCore as Qc
+import PySide6.QtWidgets as Qw
+import json
+
+def load_json(filename):
+    with open(filename, 'r', encoding='utf-8') as f: 
+        data = json.load(f) 
+    return data
+
+def save_json(filename, data):
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+data = load_json('data.json')
+button_set = (327,80)
+button_size = (60,30)
+
+class SecondaryWindow(Qw.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("サブアプリ")
+        self.setGeometry(150, 150, 540, 120)  # ウィンドウの位置とサイズ
+        
+        # 何かシンプルなラベルを追加
+        label = Qw.QLabel("変更するリスト名", self)
+        label.setGeometry(50, 20, 200, 30)
+
+        self.combo_box = Qw.QComboBox(self)
+        self.combo_box.setGeometry(50, 50, 195, 30)  # 位置とサイズを指定
+        self.combo_box.addItems(data["list_data"])
+
+        self.input_field = Qw.QLineEdit(self)
+        self.input_field.setPlaceholderText("リスト名を入力してください")
+        self.input_field.setVisible(False)
+        self.input_field.setVisible(True)
+        self.input_field.setGeometry(295,50,190,28)
+        self.input_field.setFocus()
+
+        self.btn_decide = Qw.QPushButton('決定',self)
+        self.btn_decide.setGeometry(button_set[0],button_set[1],button_size[0],button_size[1])
+        self.btn_decide.clicked.connect(self.btn_decide_clicked)
+
+        self.btn_cancel = Qw.QPushButton('中止',self)
+        self.btn_cancel.setGeometry(button_set[0]+100,button_set[1],button_size[0],button_size[1])
+        self.btn_cancel.clicked.connect(self.btn_cancel_clicked)
+
+    def btn_decide_clicked(self):
+      selected_item = self.combo_box.currentText()
+      for i in range(len(data["list_data"])):
+        if data["list_data"][i] == selected_item:
+          data["list_data"][i] = self.input_field.text()
+          print(data["list_data"])
+          save_json('data.json', data)
+          self.close()
+
+      
+    def btn_cancel_clicked(self):
+        self.close()
+
+
+
+if __name__ == '__main__':
+    app = Qw.QApplication(sys.argv)
+    main_window = SecondaryWindow()
+    main_window.show()
+    sys.exit(app.exec())
